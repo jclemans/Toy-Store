3ed009d8e87d2216b513214a2ea2199c90466b8b
Jeremy Clemans & Corey Norris <student@epicodus.com>
2014-04-11 14:39:00 -0700

    adds custom urls for login, logout, and signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Store::Application.routes.draw do
   resources :products
   resources :users
   resources :sessions
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
