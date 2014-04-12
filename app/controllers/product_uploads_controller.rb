2902906808fd0ed92fca62d0987436322796735b
Jeremy Clemans & Corey Norris <student@epicodus.com>
2014-04-11 17:11:59 -0700

    add uniqueness validation to products

--- app/controllers/product_uploads_controller.rb
+++ app/controllers/product_uploads_controller.rb
@@ -5,6 +5,7 @@ class ProductUploadsController < ApplicationController
 
   def create
     ProductUpload.new(params[:file].tempfile).parse!
+    flash[:notice] = "CSV uploaded. See additions below!"
     redirect_to root_path
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ActiveRecord::Base
   validates :name, :presence => true
   validates :price, :presence => true
   validates :description, :presence => true
-
+  validates :name, :uniqueness => true
   validates :price, :numericality => true
 end
